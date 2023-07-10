d072becc50535cd3f0f12a3072fbff38c931025d
Giogio-armani <giovan.hamadaine@hotmail.com>
2023-07-10 20:20:37 +0200

    bug mineur

--- assests/lib/effet.js
+++ assests/lib/effet.js
@@ -1,6 +1,10 @@
 let buttonAjout = document.querySelector(".btn3");
 let formAjoute = document.querySelector(".ajoutlivre");
 buttonAjout.addEventListener("click", function () {
+  document.querySelector("#isbn").value = "";
+  document.querySelector("#titre").value = "";
+  document.querySelector("#auteur").value = "";
+  document.querySelector("#editeur").value = "";
   formAjoute.classList.toggle("active");
   btnmModifer.classList.add("hidden");
   button.classList.remove("hidden");
